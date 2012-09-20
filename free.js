3ce6169351c3382fddd18dbb700c28ceeb375dac
Sakari Jokinen <sakariij@gmail.com>
2012-09-21 01:00:47 +0300

    Fresh identifier generation

---
+++ free.js
@@ -0,0 +1,19 @@
+var transform = require('./transform');
+exports.all = function(ast) {
+    var identifiers = {};
+    transform.transform(ast, function(ast) {
+			    if (ast.type === 'Identifier')
+				identifiers[ast.name] = 1;
+			    return ast;
+			});
+    return identifiers;
+};
+
+exports.fresh = function(used) {
+    for (var i = 0; used["_" + i]; i++ ) {}
+    used['_' + i] = 1;
+    return {
+	type: 'Identifier',
+	name: "_" + i
+    };
+};
---
+++ test/free.js
@@ -0,0 +1,41 @@
+var should = require('should');
+var reify = require('../reify').reify;
+var free = require('../free');
+
+describe('free', function(){
+	     describe('all', function() {
+			  it('it returns all identifiers in given ast'
+			     , function() {
+				 free.all(reify(function block() {
+						    var b;
+						    a = c + 1;
+						    function c() {
+							d;
+						    }; 
+						}))
+				     .should
+				     .eql({a: 1, b: 1, c: 1, d: 1});
+			     });
+		      });
+
+	     describe('fresh', function() {
+			  it('returns a fresh identifier'
+			    , function() {
+				free.fresh(free.all(reify(function block() {
+							       var _0;
+							   })))
+				    .should
+				    .eql({
+					     type: 'Identifier',
+					     name: '_1'
+					 });
+			    });
+			  it('adds itself to the set of used ids', function() {
+				 var used = free.all(reify(function block() {
+							       var _0;
+							   }));
+				 free.fresh(used);
+				 should.strictEqual(used._1, 1);
+			     });
+		      });
+	 });
